3ec5b675957e3e0900a53b57408dff4a84fe39c7
JacekGodzicki <jgodzicki@dps-systems.pl>
2025-09-16 13:55:56 +0200

    fix: Fix to additional pages for Towar

---
+++ DPS.MRPReport.UI/Extenders/APSMinimalStockPageExtender.cs
@@ -0,0 +1,31 @@
+﻿using DPS.MRPReport.Rows;
+using DPS.MRPReport.UI.Extenders;
+using Soneta.Business;
+using Soneta.Towary;
+
+[assembly: Worker(typeof(APSMinimalStockPageExtender))]
+
+namespace DPS.MRPReport.UI.Extenders
+{
+    internal class APSMinimalStockPageExtender
+    {
+        [Context]
+        public Towar Towar { get; set; }
+
+        public ViewInfo PositionsViewInfo
+        {
+            get
+            {
+                ViewInfo viewInfo = new ViewInfo();
+                viewInfo.CreateView += APSMinimalStockPageViewInfoCreateView;
+                return viewInfo;
+            }
+        }
+
+        private void APSMinimalStockPageViewInfoCreateView(object sender, CreateViewEventArgs args)
+        {
+            RowCondition rc = new FieldCondition.Equal($"{nameof(APSMinimalStock.Towar)}", Towar);
+            args.View = args.Session.GetDPSMRPReport().APSMinStocks.PrimaryKey[rc].CreateView();
+        }
+    }
+}
